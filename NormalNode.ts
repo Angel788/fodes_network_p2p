1abb2b809bc14f446b19900db81e7584737b33de
Angel788 <angel-yahel@outlook.com>
2026-04-02 18:04:38 -0600

    Configurando servidor

--- NormalNode.ts
+++ NormalNode.ts
@@ -13,7 +13,7 @@ const main = async () => {
         const node = await NormalNode.create();
         const nodeGateway = new NodeGateway(node)
         console.log(node.id);
-        const dir = "/ip4/136.113.56.80/tcp/1080/p2p/12D3KooWE8dYk36eQ5eZ6rnQHamfHvHVnRCLE7v3TrZviZsebEzd";//await axios.get<DireccionLibp2p>('http://192.168.1.67:8000/directionBoostrapNode');
+        const dir = '/ip4/136.113.56.80/tcp/1080/p2p/12D3KooWPzkFYx1kgvgwsD3KxyX8NdenXjTXjzbsC4dyXgPYqm7t';//await axios.get<DireccionLibp2p>('http://192.168.1.67:8000/directionBoostrapNode');
         node.contact(dir);
     } catch (error) {
         console.log(error)
