a1b16b5b8109c88664f98ad742685fbba584534a
Angel788 <angel-yahel@outlook.com>
2026-04-03 16:22:21 -0600

    Configurando Servidor

--- modules/Node/CentralNode.ts
+++ modules/Node/CentralNode.ts
@@ -1,15 +1,14 @@
-import { createLibp2p, Libp2p } from 'libp2p'
+import { createLibp2p } from 'libp2p'
 import { tcp } from '@libp2p/tcp'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { identify, identifyPush } from '@libp2p/identify'
 import { ping } from '@libp2p/ping'
-import { kadDHT, removePrivateAddressesMapper, removePublicAddressesMapper } from '@libp2p/kad-dht'
+import { kadDHT } from '@libp2p/kad-dht'
 import { NodeLibp2p } from './NodeLibp2p.js'
-import { uPnPNAT } from '@libp2p/upnp-nat'
 import { autoNAT } from '@libp2p/autonat'
 import { circuitRelayServer } from '@libp2p/circuit-relay-v2'
-import { circuitRelayTransport } from '@libp2p/circuit-relay-v2'
+
 export class CentralNode extends NodeLibp2p {
     public static async create(ip: string): Promise<CentralNode> {
         const instance = new CentralNode();
@@ -27,16 +26,17 @@ export class CentralNode extends NodeLibp2p {
                 ping: ping(),
                 dht: kadDHT({
                     protocol: '/fodes',
-                    clientMode: false,
-                    peerInfoMapper: removePublicAddressesMapper
+                    clientMode: false
                 }),
                 autoNAT: autoNAT(),
-                upnpNAT: uPnPNAT()
+                relay: circuitRelayServer()
             }
         });
         instance.id = instance.node.peerId.toString();
+
         await instance.node.start();
         await instance.nodeDb.initDb();
+
         return instance;
     }
 }
--- modules/Node/NormalNode.ts
+++ modules/Node/NormalNode.ts
@@ -1,25 +1,27 @@
-import { createLibp2p, Libp2p } from 'libp2p'
+import { createLibp2p } from 'libp2p'
 import { tcp } from '@libp2p/tcp'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { identify, identifyPush } from '@libp2p/identify'
 import { ping } from '@libp2p/ping'
-import { kadDHT, removePrivateAddressesMapper, removePublicAddressesMapper } from '@libp2p/kad-dht';
+import { kadDHT } from '@libp2p/kad-dht';
 import { bootstrap } from '@libp2p/bootstrap'
 import { NodeLibp2p } from './NodeLibp2p.js'
 import { circuitRelayTransport } from '@libp2p/circuit-relay-v2'
 import { dcutr } from '@libp2p/dcutr'
-import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { autoNAT } from '@libp2p/autonat'
-import { waitForDebugger } from 'inspector'
+
 export class NormalNode extends NodeLibp2p {
     static async create(ip: string, bootstrapIp: string): Promise<NormalNode> {
         const instance = new NormalNode();
         instance.node = await createLibp2p({
             addresses: {
-                listen: ['/ip4/0.0.0.0/tcp/1080']
+                listen: ['/ip4/0.0.0.0/tcp/0', '/ip6/::/tcp/0']
             },
-            transports: [tcp()],
+            transports: [
+                tcp(),
+                circuitRelayTransport()
+            ],
             streamMuxers: [yamux()],
             connectionEncrypters: [noise()],
             services: {
@@ -28,15 +30,17 @@ export class NormalNode extends NodeLibp2p {
                 ping: ping(),
                 dht: kadDHT({
                     protocol: '/fodes',
-                    clientMode: false,
-                    peerInfoMapper: removePublicAddressesMapper
+                    clientMode: false
                 }),
+                autonat: autoNAT(),
+                dcutr: dcutr()
             }
         });
         instance.id = instance.node.peerId.toString();
+
         await instance.node.start();
-        //await instance.waitForPeers();
-        await instance.nodeDb.initDb();
+        instance.nodeDb.initDb();
+
         return instance;
     }
 }
