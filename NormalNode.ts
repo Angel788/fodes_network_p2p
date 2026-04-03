7e033a6f8d2cd0b935c613bb5ead681318cb91b6
Angel788 <angel-yahel@outlook.com>
2026-04-03 16:00:12 -0600

    Configurando Servidor

--- NormalNode.ts
+++ NormalNode.ts
@@ -11,7 +11,7 @@ interface DireccionLibp2p {
 const main = async () => {
     try {
         const ip6 = await getLocalIPv6()
-        const dir = '/ip4/34.71.122.91/tcp/1080/p2p/12D3KooWAFSsT8rW2pJUYLiEeRrZBPGaB45nQiWFf57DtJYvW3vo';//await axios.get<DireccionLibp2p>('http://192.168.1.67:8000/directionBoostrapNode');
+        const dir = '/ip4/34.71.122.91/tcp/1080/p2p/12D3KooWJLmUH7wFoSTqJXTJ5ek9UbqedVPhjbRQ7zbLchhh8PbX';//await axios.get<DireccionLibp2p>('http://192.168.1.67:8000/directionBoostrapNode');
         const node = await NormalNode.create(ip6, dir);
         const nodeGateway = new NodeGateway(node)
         console.log(node.getDirections());
