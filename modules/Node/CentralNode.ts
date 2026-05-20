d416b672533e29d53fa7606dd6fd3da09d63699c
m1chaelj <jahicco24@gmail.com>
2026-05-20 13:53:41 -0600

    Peer equivalente en desktop

--- modules/Node/CentralNode.ts
+++ modules/Node/CentralNode.ts
@@ -9,15 +9,34 @@ import { NodeLibp2p } from './NodeLibp2p.js'
 import { autoNAT } from '@libp2p/autonat'
 import { circuitRelayServer } from '@libp2p/circuit-relay-v2'
 import { preSharedKey } from '@libp2p/pnet'
+import { keys } from '@libp2p/crypto'
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from 'node:fs'
+import path from 'node:path'
+
+async function loadOrCreatePrivateKey(dbPath: string = '.db'): Promise<Awaited<ReturnType<typeof keys.generateKeyPair>>> {
+    mkdirSync(path.resolve(dbPath), { recursive: true })
+    const keyPath = path.join(path.resolve(dbPath), 'peer.key')
+    if (existsSync(keyPath)) {
+        const buf = readFileSync(keyPath)
+        return keys.privateKeyFromProtobuf(buf) as any
+    }
+    const privateKey = await keys.generateKeyPair('Ed25519')
+    writeFileSync(keyPath, keys.privateKeyToProtobuf(privateKey))
+    console.log('[Bootstrap] Nueva clave generada y guardada. Peer ID estable en futuros reinicios.')
+    return privateKey
+}
+
 export class CentralNode extends NodeLibp2p {
     public static async create(ip4: string, psk: Uint8Array, ip6: string | null = null): Promise<CentralNode> {
         const instance = new CentralNode();
+        const privateKey = await loadOrCreatePrivateKey();
 
         const listenAddrs  = ['/ip4/0.0.0.0/tcp/1080', '/ip6/::/tcp/1080'];
         const announceAddrs = ['/ip4/' + ip4 + '/tcp/1080'];
         if (ip6) announceAddrs.push('/ip6/' + ip6 + '/tcp/1080');
 
         instance.node = await createLibp2p({
+            privateKey,
             addresses: {
                 listen:   listenAddrs,
                 announce: announceAddrs
